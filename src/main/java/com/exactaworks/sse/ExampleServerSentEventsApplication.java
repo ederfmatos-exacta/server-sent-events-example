2d7c9b6dcc4656ccf1d6e8f49239c8108dadbee4
Eder Matos <3rd.eder.dmatos@amedigital.com>
2022-09-23 16:30:08 -0300

    feat: create basic project

---
+++ src/main/java/com/exactaworks/sse/ExampleServerSentEventsApplication.java
@@ -0,0 +1,13 @@
+package com.exactaworks.sse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleServerSentEventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleServerSentEventsApplication.class, args);
+    }
+
+}
